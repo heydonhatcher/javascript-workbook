a41bc4c782cb64e138944086f69d3642d900e870
heydonhatcher <heyhatch@gmail.com>
2019-10-22 13:24:42 -0500

    changed a little

--- 06week/hackathon.js
+++ 06week/hackathon.js
@@ -1,28 +1,31 @@
 function rampNumberCount(number) {
-  var output = [],
-    sNumber = number.toString();
-
-  for (var i = 0, len = sNumber.length; i < len; i++) {
-    output.push(sNumber[i]);
-  }
-
-  console.log("output:", output);
   var rampCount = 0;
-  for (var i = 0; i < output.length; i++) {
-    if (output[i] > output[i + 1]) {
-      console.log("Not a ramp number");
-      return;
-    } else if (output[i] === output[i + 1]) {
-      console.log("currentHighest same as next digit, no change");
-    } else if (output[i] < output[i + 1]) {
-      currentHighest = output[i + 1];
-      console.log("new currentHighest is", currentHighest);
-      // var currentHighest = output[i];
-      // console.log("currentHighest:", currentHighest);
-    } else {
-      rampCount++;
+  for (var currentNum = number - 1; currentNum > -1; currentNum--) {
+    var output = [],
+      sNumber = currentNum.toString();
+
+    for (var i = 0, len = sNumber.length; i < len; i++) {
+      output.push(sNumber[i]);
     }
-    console.log("current rampCount:", rampCount);
+
+    console.log("output:", output);
+    var rampCount = 0;
+    for (var i = 0; i < output.length; i++) {
+      if (output[i] > output[i + 1]) {
+        console.log("Not a ramp number");
+      } else if (output[i] === output[i + 1]) {
+        console.log("currentHighest same as next digit, no change");
+      } else if (output[i] < output[i + 1]) {
+        currentHighest = output[i + 1];
+        console.log("new currentHighest is", currentHighest);
+        // var currentHighest = output[i];
+        // console.log("currentHighest:", currentHighest);
+      } else {
+        rampCount++;
+      }
+      console.log("current rampCount:", rampCount);
+    }
   }
+  console.log("ramp numbers less than", number, ":", rampCount);
 }
-rampNumberCount(256789);
+rampNumberCount(2567);
