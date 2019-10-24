b3a76c5fd3748260af60989750bf7f6e0a67d6b6
heydonhatcher <heyhatch@gmail.com>
2019-10-23 19:07:32 -0500

    ready

--- 06week/hackathon.js
+++ 06week/hackathon.js
@@ -3,23 +3,21 @@ function rampNumberCount(number) {
   for (var currentNum = number - 1; currentNum > -1; currentNum--) {
     var output = [],
       sNumber = currentNum.toString();
-
     for (var i = 0, len = sNumber.length; i < len; i++) {
       output.push(sNumber[i]);
     }
     console.log("---------------\n", "output:", output);
-
     for (var i = 1; i <= output.length; i++) {
       if (output.length === 1) {
         rampCount++;
-        console.log("Is ramp number: only one digit");
+        console.log("Is a ramp number: Only one digit");
         console.log("(changed) current rampCount:", rampCount);
         break;
       } else if (output[i - 1] > output[i]) {
         console.log("Not a ramp number");
         console.log("(unchanged) current rampCount:", rampCount);
         break;
-      } else if (output[i - 1] === output[1] || output[i - 1] < output[i]) {
+      } else if (output[i - 1] === output[i] || output[i - 1] < output[i]) {
         if (i + 1 === output.length) {
           rampCount++;
           console.log("Is a ramp number");
@@ -28,7 +26,6 @@ function rampNumberCount(number) {
       }
     }
   }
-  console.log("ramp numbers less than", number, ":", rampCount);
+  console.log("Ramp numbers less than", number, ":", rampCount);
 }
-
-rampNumberCount(100);
+rampNumberCount(99999);
