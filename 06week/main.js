3ab824ab3a74beaf76fa96b921895fdeabc448d9
heydonhatcher <heyhatch@gmail.com>
2019-10-28 19:58:15 -0500

    got tests in

--- 06week/main.js
+++ 06week/main.js
@@ -1,3 +1,5 @@
+//const assert = require("assert");
+
 const arrOfPeople = [
   {
     id: 2,
@@ -79,17 +81,17 @@ class player {
 class blueTeammate {
   constructor(person) {
     for (var [key, value] of Object.entries(person)) {
-      console.log("key", key, "value", value);
+      //console.log("key", key, "value", value);
       this[key] = value;
     }
     this.teamColor = "blue";
-    this.mascot = "rattlin' rattlesnakes";
+    this.mascot = "rattlin rattlesnakes";
   }
 }
 class redTeammate {
   constructor(person) {
     for (var [key, value] of Object.entries(person)) {
-      console.log("key", key, "value", value);
+      //console.log("key", key, "value", value);
       this[key] = value;
     }
     this.teamColor = "red";
@@ -97,17 +99,24 @@ class redTeammate {
   }
 }
 
-const addPlayerToTeam = (person, color) => {
+const addPlayerToTeam = (player, color, browser = true) => {
   if (color === "blue") {
-    let bluePlayer = new blueTeammate(person);
+    let bluePlayer = new blueTeammate(player);
     console.log("bluePlayer:", bluePlayer);
+    console.log("bluePlayer.teamColor", bluePlayer.teamColor);
     blueTeam.push(bluePlayer);
-    listTeamMembers("blue", blueTeam);
+    if (browser) {
+      listTeamMembers("blue", blueTeam);
+    }
+    return bluePlayer;
   } else if (color === "red") {
-    let redPlayer = new redTeammate(person);
+    let redPlayer = new redTeammate(player);
     console.log("redPlayer:", redPlayer);
     redTeam.push(redPlayer);
-    listTeamMembers("red", redTeam);
+    if (browser) {
+      listTeamMembers("red", redTeam);
+    }
+    return redPlayer;
   }
   console.log("blueTeam:", blueTeam, "- redTeam:", redTeam);
 };
@@ -128,10 +137,10 @@ const listTeamMembers = (color, team) => {
   listOfPlayers.splice(listOfPlayers.indexOf(player), 1);
 
   var clickedButton = event.target;
-  console.log("event target:", clickedButton);
+  //console.log("event target:", clickedButton);
   var clickedDiv = clickedButton.parentElement;
   var newList = clickedDiv.parentElement;
-  console.log("newList:", newList);
+  //console.log("newList:", newList);
   clickedDiv.removeChild(clickedButton);
   newList.removeChild(clickedDiv);
 };
@@ -159,7 +168,10 @@ const listPeopleChoices = () => {
 };
 
 const listPlayerChoices = () => {
-  const listElement = document.getElementById("players");
+  const playersElement = document.getElementById("players");
+  while (playersElement.firstChild) {
+    playersElement.removeChild(playersElement.firstChild);
+  }
   listOfPlayers.forEach(person => {
     const li = document.createElement("li");
     const buttonOne = document.createElement("button");
@@ -174,50 +186,55 @@ const listPlayerChoices = () => {
     li.appendChild(
       document.createTextNode(person.name + " - " + person.skillSet)
     );
-    listElement.append(li);
+    playersElement.append(li);
     buttonTwo.addEventListener("click", function() {
       addPlayerToTeam(person, "red");
     });
     li.appendChild(buttonOne);
     li.appendChild(buttonTwo);
-    listElement.append(li);
+    playersElement.append(li);
   });
 };
 
-const makePlayer = person => {
-  console.log(`li ${person.id} was clicked!`);
+const makePlayer = (person, browser = true) => {
+  //console.log(`li ${person.id} was clicked!`);
   let _player = new player(person, 1, 1, 1, 1, 1);
-  console.log(arrOfPeople.indexOf(person));
+  //console.log(arrOfPeople.indexOf(person));
   let poppedPlayer = arrOfPeople.splice(arrOfPeople.indexOf(person), 1);
-  console.log("poppedPlayer:", poppedPlayer);
-  console.log("arrOfPeople:", arrOfPeople);
+  //console.log("poppedPlayer:", poppedPlayer);
+  //console.log("arrOfPeople:", arrOfPeople);
   listOfPlayers.push(_player);
-  console.log("final player object:", _player);
-  console.log("listOfPlayers:", listOfPlayers);
-  var clickedButton = event.target;
-  console.log("event target:", clickedButton);
-  var clickedDiv = clickedButton.parentElement;
-  var newList = clickedDiv.parentElement;
-  console.log("newList:", newList);
-  clickedDiv.removeChild(clickedButton);
-  newList.removeChild(clickedDiv);
-  listPlayerChoices();
-  return newList;
+  //console.log("final player object:", _player);
+  //console.log("listOfPlayers:", listOfPlayers);
+  if (browser) {
+    var clickedButton = event.target;
+    //console.log("event target:", clickedButton);
+    var clickedDiv = clickedButton.parentElement;
+    var newList = clickedDiv.parentElement;
+    //console.log("newList:", newList);
+    clickedDiv.removeChild(clickedButton);
+    newList.removeChild(clickedDiv);
+    listPlayerChoices();
+  }
+  //return newList;
+  return _player;
 };
 
-// if (typeof describe === "function") {
-//   describe("#makePlayer()", () => {
-//     it("should turn person into player", () => {
-//       assert.equal(makePlayer(arrOfPeople[0]).length, arrOfPeople.length - 1);
-//       //assert.equal(makePlayer("dog"), "ogday");
-//     });
-//     it("should translate a complex word", () => {
-//       assert.equal(pigLatin("create"), "eatecray");
-//       assert.equal(pigLatin("valley"), "alleyvay");
-//     });
-//     it('should attach "yay" if word begins with vowel', () => {
-//       assert.equal(pigLatin("egg"), "eggyay");
-//       assert.equal(pigLatin("emission"), "emissionyay");
-//     });
-//   });
-// }
+if (typeof describe === "function") {
+  describe("#addPlayerToTeam()", () => {
+    it("should instantiate player object from element of arrOfPeople", () => {
+      var madePlayer = makePlayer(arrOfPeople[3], false);
+      assert.equal(madePlayer.canDodgeBall, 1);
+    });
+    it("should instantiate player object 'bluePlayer'", () => {
+      var testPlayer = makePlayer(arrOfPeople[0], false);
+      var testTeamMate = addPlayerToTeam(testPlayer, "blue", false);
+      assert.equal(testTeamMate.teamColor, "blue");
+    });
+    it("should instantiate player object 'redPlayer'", () => {
+      var testPlayer = makePlayer(arrOfPeople[1], false);
+      var testTeamMate = addPlayerToTeam(testPlayer, "red", false);
+      assert.equal(testTeamMate.teamColor, "red");
+    });
+  });
+}
